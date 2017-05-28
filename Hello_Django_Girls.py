4c27c25e8bcc0d416a261ef5a1b13cf5ad46449f
Monika Janowska <monika.janowskaa@gmail.com>
2017-05-28 14:31:04 +0200

    My Django Girls app, first commit

---
+++ Hello_Django_Girls.py
@@ -0,0 +1,15 @@
+
+if 3 >2:
+	print('To działa!')
+if 5 > 2:
+	print('5 jest wieksze od 2')
+else:
+	print ('5 nie jest wieksze od 2')
+name = 'Monika'
+if name == 'Ola':
+	print('Hej Ola!')
+elif name == 'Monika':
+    print ('Hej Monika!')
+else:
+	print('Hej nieznajoma!')
+	
